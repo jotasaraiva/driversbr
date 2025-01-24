ce2244455a147302d96ca7b3c41aaf9a8b5c8bcf
Pedro Augusto Borges dos Santos <pabsantos21@gmail.com>
2025-01-24 11:47:15 -0300

    fix: update documentation for licensed drivers dataset

--- R/data.R
+++ R/data.R
@@ -1,18 +1,17 @@
-#' Brazilian Licensed Drivers Data
+#' Licensed Drivers Data
 #'
 #' A dataset of licensed drivers data scraped from the
-#' Brazilian Ministry of Transport
+#' Brazilian Ministry of Transport.
 #'
-#' @format ## `drivers`
-#' A `tibble` data frame encoded to Brazilian Portuguese:
+#' @format A tibble with 114,492 rows and 7 variables:
 #' \describe{
-#'    \item{regiao}{Brazilian Geographic Region}
-#'    \item{uf}{Federative Unit - Brazilian States Acronym}
-#'    \item{sexo}{Drivers sex}
-#'    \item{faixa_etaria}{Drivers age group}
-#'    \item{ano}{Year}
-#'    \item{categoria_cnh}{Drivers license category}
-#'    \item{condutores}{Amount of drivers}
+#'   \item{regiao}{Brazilian Geographic Region}
+#'   \item{uf}{Federative Unit - Brazilian States Acronym}
+#'   \item{sexo}{Drivers sex}
+#'   \item{faixa_etaria}{Drivers age group}
+#'   \item{ano}{Year}
+#'   \item{categoria_cnh}{Drivers license category}
+#'   \item{condutores}{Amount of drivers}
 #' }
-#' @source https://www.gov.br/transportes/pt-br/assuntos/transito/conteudo-Senatran/estatisticas-quantidade-de-habilitados-denatran
+#' @source \url{https://www.gov.br/transportes/pt-br/assuntos/transito/conteudo-Senatran/estatisticas-quantidade-de-habilitados-denatran}
 "drivers"
